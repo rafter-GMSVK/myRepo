f4b68f4972c94c00ae2c191640724dd5a400c699
rafter-GMSVK <shanbhagji@gmail.com>
2022-09-22 23:18:30 +0530

    adding useState function to count

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,14 @@
-console.log("hello");
-console.log("Today is a good day")
+import React from 'react'
+import { useState } from 'react'
+const HelloWorld = () => {
+    const [count, setCount] = useState(0);
+  return (
+    <div>
+        <button onClick={()=>setCount(count+1)}>Increase</button>
+        {count}
+        <button onClick={()=>{setCount(count-1)}}>Decrease</button>
+    </div>
+  )
+}
+
+export default HelloWorld
